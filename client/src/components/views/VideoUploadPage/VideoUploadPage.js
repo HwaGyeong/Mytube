df4ebe4bf6ac8703b756839676f17c0abc6864fe
HwaGyeong <dlghkrud18@naver.com>
2021-06-02 20:17:54 +0900

    multer 동영상 업로드 기능 추가

--- client/src/components/views/VideoUploadPage/VideoUploadPage.js
+++ client/src/components/views/VideoUploadPage/VideoUploadPage.js
@@ -1,10 +1,12 @@
 import React,{useState} from 'react'
 import { Typography, Button, Form, message, Input, Icon } from 'antd';
 import Dropzone from 'react-dropzone';
-
+import axios from 'axios';
 const { Title } = Typography;
 const { TextArea } = Input;
 
+
+//option 각각 값 할당후 map으로 출력
 const PrivateOptions=[
     {value:0,label:"Private"},
     {value:1,label:"Public"}
@@ -24,7 +26,7 @@ function VideoUploadPage() {
     const [Private,setPrivate]=useState(0)
     const [Category,setCategory]=useState("File & Animation")
 
-    const onTitleChange = (e)=> {
+    const onTitleChange = (e)=> {//입력창에 받아들이도록 하는 함수
         setVideoTitle(e.currentTarget.value)
     }
     const onDescriptionChange =(e)=>{
@@ -36,6 +38,27 @@ function VideoUploadPage() {
     const onPrivateChange=(e)=>{
         setPrivate(e.currentTarget.value)
     }
+    const onDrop=(files)=>{//파일 추가 함수
+        let formData = new FormData;
+        const config= {
+                header: {'conent-type':'multipart/form-data'}//오류 방지를 위해 필요함 
+        }
+        formData.append("file",files[0])
+        console.log(files)
+
+        axios.post('/api/video/uploadfiles',formData,config)
+        .then(response=> {
+            if(response.data.success)
+            {
+
+            }
+            else
+            {
+                alert('비디오 업로드를 실패했습니다');
+            }
+        })
+    }
+
     return (
         <div style={{maxWidth:'700px',margin:'2rem auto'}}>       
             <div style={{textAlign:'center',marginBottom:'2rem'}}>
@@ -44,9 +67,9 @@ function VideoUploadPage() {
             <Form onSubmit>
             <div style={{ display: 'flex', justifyContent: 'space-between' }}>
                     <Dropzone
-                        onDrop
-                        multiple
-                        maxSize>
+                        onDrop={onDrop}
+                        multiple={false}
+                        maxSize={1000000}>
                         {({ getRootProps, getInputProps }) => (
                             <div style={{ width: '300px', height: '240px', border: '1px solid lightgray', display: 'flex', alignItems: 'center', justifyContent: 'center' }}
                                 {...getRootProps()}
